6876c04e6fa5720f20b2d627b8b11be3bb4b74f4
Riccardo Moschetti <riccardo.moschetti@outlook.com>
2023-04-01 16:42:40 +0200

    renamed and repaired bash file

--- src/Functions/AllFunctions.cs
+++ src/Functions/AllFunctions.cs
@@ -2,52 +2,52 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System;
 using System.Net;
+using System.Runtime.ConstrainedExecution;
 
-namespace APIsault.Azure.Servers.Functions
-{
-    public class AllFunctions
-    {
+namespace APIsault.Azure.Servers.Functions {
+    public class AllFunctions {
         private readonly ILogger _logger;
-
-        public AllFunctions(ILoggerFactory loggerFactory)
-        {
+        public AllFunctions(ILoggerFactory loggerFactory) {
             _logger = loggerFactory.CreateLogger<AllFunctions>();
         }
 
+        private static Random random = new Random();
+        private object MakePerson() {
+            return new {
+                id = Guid.NewGuid().ToString(),
+                name = new string(Enumerable.Repeat("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 20).Select(s => s[random.Next(s.Length)]).ToArray())
+            };
+        }
+  
+
         [Function("SimpleJson")]
-        public HttpResponseData SimpleJson([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
-        {
+        public HttpResponseData SimpleJson([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req) {
             _logger.LogInformation("Entered the SimpleJson API");
-
-            object Person = new { id = 12, Name = "Ciccio" };
-            string jsonToReturn = JsonConvert.SerializeObject(Person);
-
+            string jsonToReturn = JsonConvert.SerializeObject(MakePerson());
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "application/json; charset=utf-8");
-            response.WriteString(jsonToReturn); 
+            response.WriteString(jsonToReturn);
             _logger.LogInformation("Leaving the SimpleJson API");
 
             return response;
         }
 
         [Function("JustWait")]
-        public async Task<HttpResponseData> JustWait([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
-        {
+        public async Task<HttpResponseData> JustWait([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req) {
             _logger.LogInformation("Entered the JustWait API");
             var query = System.Web.HttpUtility.ParseQueryString(req.Url.Query);
 
             HttpResponseData responseData;
 
             int waitMS = 0;
-            if (query.Count != 0 && query["waitMS"] != null && int.TryParse(query["waitMS"], out waitMS))
-            {
-                 _logger.LogInformation("Now waiting...");
+            if (query.Count != 0 && query["waitMS"] != null && int.TryParse(query["waitMS"], out waitMS)) {
+                _logger.LogInformation("Now waiting...");
                 await Task.Delay(waitMS);
                 _logger.LogInformation("Wait is over!");
 
-                object Person = new { id = 12, Name = "Ciccio" };
-                string jsonToReturn = JsonConvert.SerializeObject(Person);
+                string jsonToReturn = JsonConvert.SerializeObject(MakePerson());
 
                 responseData = req.CreateResponse(HttpStatusCode.OK);
                 responseData.Headers.Add("Content-Type", "application/json; charset=utf-8");
@@ -56,8 +56,7 @@ namespace APIsault.Azure.Servers.Functions
                 _logger.LogInformation("Leaving the JustWait API");
                 return responseData;
             }
-            else
-            {
+            else {
                 responseData = req.CreateResponse(HttpStatusCode.BadRequest);
                 responseData.WriteString("waitMS query parameter is incorrect");
                 return responseData;
@@ -66,22 +65,19 @@ namespace APIsault.Azure.Servers.Functions
 
 
         [Function("JustWaitWithoutAsync")]
-        public  HttpResponseData JustWaitWithoutAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
-        {
+        public HttpResponseData JustWaitWithoutAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req) {
             _logger.LogInformation("Entered the JustWaitWithoutAsync API");
             var query = System.Web.HttpUtility.ParseQueryString(req.Url.Query);
 
             HttpResponseData responseData;
 
             int waitMS = 0;
-            if (query.Count != 0 && query["waitMS"] != null && int.TryParse(query["waitMS"], out waitMS))
-            {
+            if (query.Count != 0 && query["waitMS"] != null && int.TryParse(query["waitMS"], out waitMS)) {
                 _logger.LogInformation("Now waiting...");
                 Task.Delay(waitMS);
                 _logger.LogInformation("Wait is over!");
 
-                object Person = new { id = 12, Name = "Ciccio" };
-                string jsonToReturn = JsonConvert.SerializeObject(Person);
+                string jsonToReturn = JsonConvert.SerializeObject(MakePerson());
 
                 responseData = req.CreateResponse(HttpStatusCode.OK);
                 responseData.Headers.Add("Content-Type", "application/json; charset=utf-8");
@@ -90,8 +86,7 @@ namespace APIsault.Azure.Servers.Functions
                 _logger.LogInformation("Leaving the JustWaitWithoutAsync API");
                 return responseData;
             }
-            else
-            {
+            else {
                 responseData = req.CreateResponse(HttpStatusCode.BadRequest);
                 responseData.WriteString("waitMS query parameter is incorrect");
                 return responseData;
---
+++ src/performanceStresser.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# usage: bash ./performanceStresser 2>&1 | tee -a "peformanceResults.txt"
+
+app_id="{your app id}"
+password="{your app secret}"
+tenant_id="{your tenant id}"
+
+resource_group="{the resource group of your api}"
+app_plan="{the app service name of your api}"
+function="{the name of your function}"
+
+output_file_prefix="{output file name prefix}"
+duration="600s"
+
+ulimit -m 1000000
+ulimit -n 1000000
+
+for plan in S1 S2 S3 P1V2 P2V2 P3V2 P1V3 P2V3 P3V3 B1
+do
+    echo "Time is: $(date)"
+	echo "logging in and restarting function"
+	az login --service-principal -u ${app_id} -p ${password} --tenant ${tenant_id}
+    az functionapp restart -n ${function} -g ${resource_group}
+    echo "sleeping 30s"
+    sleep 30s
+
+    echo  "moving to plan: ${plan}"
+	az appservice plan update -n ${app_plan} -g ${resource_group} --sku $plan
+	echo "sleeping 30s"
+    sleep 30s
+
+    echo "restarting function again"
+    az functionapp restart -n ${function} -g ${resource_group}
+	echo "sleeping 30s"
+    sleep 30s
+
+	echo "curl-ing"
+	curl https://${function}.azurewebsites.net/api/SimpleJson
+	echo "sleeping 30s"
+	sleep 30s
+
+	for i in 10 20 30 40 50 75 100 125 150 175 200 250 300 350 400 450 500 550 600 650 700 750 800 850 900 950 1000 1050 1100 1150 1200 1250 1300 1350 1400
+	do
+		echo "start: $(date)"
+		echo "testing rate $i with plan $plan"
+		output=$(echo "GET https://${function}.azurewebsites.net/api/SimpleJson" | vegeta attack -duration=${duration}  -rate $i -timeout 30s | tee ${output_file_prefix}_${plan}_rate_${i}.bin | vegeta report)
+		echo "this was the output of the test:"
+		echo $output
+		
+		cat ${output_file_prefix}_${plan}_rate_${i}.bin  | vegeta plot > ${output_file_prefix}_${plan}_rate_${i}.html
+		success_ratio=$(echo $output | grep -oP 'Success \[ratio\] \K\d+\.\d+')
+		echo "success ratio: $success_ratio"
+		
+		if (( $(echo "$success_ratio < 98" | bc -l) )); then
+			echo "success ratio is lower than 98%. Exiting the loop for this plan"
+			break
+		else 
+			echo "sleeping 60s and going on"
+			sleep 60s
+		fi
+
+		echo "finish: $(date)"
+		echo "sleeping 30s"
+		sleep 30s
+	done
+done
